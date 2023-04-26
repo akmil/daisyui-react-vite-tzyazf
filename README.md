fd879e2dbd23896851068d19ecb1077b471f8447
akmil <pashades@gmail.com>
2023-04-26 18:12:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-vite-tzyazf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-vite-tzyazf)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      foo bar
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;
